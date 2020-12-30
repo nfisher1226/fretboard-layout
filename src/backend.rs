c87a8d3fdddeac7862c424384ba9c6d10e2dc048
Nathan Fisher <nfisher.sr@gmail.com>
2020-12-29 23:32:34 -0500

    Simplify main()

--- src/backend.rs
+++ src/backend.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::all, clippy::pedantic)]
+use clap::ArgMatches;
 use crate::fretboard::Lengths;
 use rug::ops::Pow;
 use std::process::Command;
@@ -211,5 +212,74 @@ impl Specs {
                 }
             }
         }
+    }
+}
+
+pub fn run(matches: ArgMatches) {
+    match matches.subcommand() {
+        Some(("cli", cli_matches)) => {
+            let scale: f64 = match cli_matches.value_of_t("SCALE") {
+                Ok(c) => c,
+                Err(e) => {
+                    eprintln!("{}", e);
+                    process::exit(1);
+                },
+            };
+            let specs = Specs {
+                scale,
+                count: match cli_matches.value_of_t("COUNT") {
+                    Ok(c) => c,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        process::exit(1);
+                    },
+                },
+                multi: cli_matches.occurrences_of("MULTI") > 0,
+                scale_treble: if cli_matches.occurrences_of("MULTI") > 0 {
+                    match cli_matches.value_of_t("MULTI") {
+                        Ok(c) => c,
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            process::exit(1);
+                        },
+                    }
+                } else {
+                    scale
+                },
+                nut: match cli_matches.value_of_t("NUT") {
+                    Ok(c) => c,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        process::exit(1);
+                    },
+                },
+                bridge: match cli_matches.value_of_t::<f64>("BRIDGE") {
+                    Ok(c) => c + 6.0,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        process::exit(1);
+                    },
+                },
+                pfret: match cli_matches.value_of_t("PERPENDICULAR") {
+                    Ok(c) => c,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        process::exit(1);
+                    },
+                },
+                output: cli_matches.value_of("OUTPUT").unwrap().to_string(),
+                border: match cli_matches.value_of_t("BORDER") {
+                    Ok(c) => c,
+                    Err(e) => {
+                        eprintln!("{}", e);
+                        process::exit(1);
+                    },
+                },
+                external: cli_matches.occurrences_of("EXTERN") > 0,
+                cmd: cli_matches.value_of("EXTERN").unwrap().to_string(),
+            };
+            specs.run();
+        }
+        _ => crate::gui::run_gui(),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,76 +8,9 @@ mod backend;
 mod fretboard;
 mod gui;
 use backend::Specs;
-use std::process;
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).version(crate_version!()).get_matches();
-
-    match matches.subcommand() {
-        Some(("cli", cli_matches)) => {
-            let scale: f64 = match cli_matches.value_of_t("SCALE") {
-                Ok(c) => c,
-                Err(e) => {
-                    eprintln!("{}", e);
-                    process::exit(1);
-                },
-            };
-            let specs = Specs {
-                scale,
-                count: match cli_matches.value_of_t("COUNT") {
-                    Ok(c) => c,
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        process::exit(1);
-                    },
-                },
-                multi: cli_matches.occurrences_of("MULTI") > 0,
-                scale_treble: if cli_matches.occurrences_of("MULTI") > 0 {
-                    match cli_matches.value_of_t("MULTI") {
-                        Ok(c) => c,
-                        Err(e) => {
-                            eprintln!("{}", e);
-                            process::exit(1);
-                        },
-                    }
-                } else {
-                    scale
-                },
-                nut: match cli_matches.value_of_t("NUT") {
-                    Ok(c) => c,
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        process::exit(1);
-                    },
-                },
-                bridge: match cli_matches.value_of_t::<f64>("BRIDGE") {
-                    Ok(c) => c + 6.0,
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        process::exit(1);
-                    },
-                },
-                pfret: match cli_matches.value_of_t("PERPENDICULAR") {
-                    Ok(c) => c,
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        process::exit(1);
-                    },
-                },
-                output: cli_matches.value_of("OUTPUT").unwrap().to_string(),
-                border: match cli_matches.value_of_t("BORDER") {
-                    Ok(c) => c,
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        process::exit(1);
-                    },
-                },
-                external: cli_matches.occurrences_of("EXTERN") > 0,
-                cmd: cli_matches.value_of("EXTERN").unwrap().to_string(),
-            };
-            specs.run();
-        }
-        _ => gui::run_gui(),
-    }
+    backend::run(matches);
 }
